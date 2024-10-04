551dbbe0d8aea4e7a9dab15975dd83f4be4683a8
Darki21 <99948544+Darki21@users.noreply.github.com>
2024-10-04 15:47:52 +0300

    Start

---
+++ lab1/Temp/Includes/Program/Variablesvar.h
@@ -0,0 +1,41 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1727444580_4_
+#define _BUR_1727444580_4_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_BUR_LOCAL float speed1;
+_BUR_LOCAL float counter;
+_BUR_LOCAL struct FB_Motor fb_motor2;
+_BUR_LOCAL float speed;
+_BUR_LOCAL plcbit enable;
+_BUR_LOCAL struct FB_Regulator fb_controller;
+_BUR_LOCAL struct FB_Integrator integrator;
+_BUR_LOCAL struct FB_Motor fb_motor;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Program/Variables.var\\\" scope \\\"local\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MotorCont/MotorCont.fun\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1727444580_4_ */
+
---
+++ lab1/Temp/Includes/lab1/AsDefault.h
@@ -0,0 +1,8 @@
+#ifndef _LAB1_DEFAULT_1000760196
+#define _LAB1_DEFAULT_1000760196
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <typesTYP.h>
+#include <variablesVAR.h>
+#endif
